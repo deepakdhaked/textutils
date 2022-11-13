b9fdeb90100d6d48e93e646db5b09488e62206c5
deepak dhaked <deepakdhakedpc@gmail.com>
2022-11-13 05:46:25 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React, { useState } from 'react';
+
+import './App.css';
+import Alert from './components/Alert';
+import About from './components/About';
+import Navbar from './components/Navbar';
+import TextForm from './components/TextForm';
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+function App() {
+  const [mode, setMode] = useState('light');
+  const [alert, setAlert] = useState(null);
+
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type
+    })
+  }
+
+  const toggleMode = () => {
+    if (mode === 'light') {
+      setMode('dark');
+      document.body.style.backgroundColor = 'black';
+      showAlert("Dark Mode has been Enbled", "success");
+    }
+    else {
+      setMode('light');
+      document.body.style.backgroundColor = 'white';
+      showAlert("Light Mode has been Enbled", "success");
+
+
+    }
+  }
+
+
+  return (
+    <>
+      <Router>
+        <Navbar title="TextUtils" aboutText="About" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <div className="container my-3">
+          <Routes>
+            <Route exact path="/about" element={<About />}></Route>
+            <Route exact path="/" element={<TextForm heading="Enter the text to analyze below" mode={mode} />}></Route>
+          </Routes>
+        </div>
+      </Router>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/TextForm.js
@@ -0,0 +1,64 @@
+import React, { useState } from 'react'
+
+export default function TextForm(props) {
+    const handleUpClick = () => {
+        let newText = text.toLocaleUpperCase();
+        setText(newText);
+    }
+
+    const handleLowClick = () => {
+        let newText = text.toLocaleLowerCase();
+        setText(newText);
+    }
+
+    const handleClearClick = () => {
+        let newText = '';
+        setText(newText);
+    }
+
+    const handleCopyClick = () => {
+        navigator.clipboard.writeText(text);
+        alert("Text is Copied Successfully");
+    }
+
+    const handleExtraSpace = () => {
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "));
+    }
+
+    // const handleSpeechClick = () => {
+    //     let msg = new SpeechSynthesisUtterance(text)
+    //     window.speechSynthesis.speak(msg);
+    // }
+
+    const handleOnChange = (event) => {
+        setText(event.target.value);
+    }
+
+    const [text, setText] = useState("");
+    return (
+        <>
+            <div className="container" style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
+                <h1>{props.heading}</h1>
+                <div className="mb-3">
+                    <textarea className="form-control" value={text} onChange={handleOnChange} style={{ backgroundColor: props.mode === 'dark' ? 'black' : 'white', color: props.mode === 'dark' ? 'white' : 'black' }} id="myBox" rows="8"></textarea>
+                </div>
+                <button type="button" className="btn btn-primary mx-2" onClick={handleUpClick}>Convert to UpperCase</button>
+                <button type="button" className="btn btn-primary mx-2" onClick={handleLowClick}>Convert to LowerCase</button>
+                <button type="button" className="btn btn-primary mx-2" onClick={handleExtraSpace}>Remove Extra Spaces</button>
+                {/* <button type="button" className="btn btn-primary mx-2" onClick={handleSpeechClick}>Text to Speech</button> */}
+                <button type="button" className="btn btn-primary mx-2" onClick={handleCopyClick}>Copy to ClipBoard</button>
+                <button type="button" className="btn btn-danger mx-2" onClick={handleClearClick}>Clear Text</button>
+
+            </div>
+
+            <div className="container my-3" style={{ color: props.mode === 'dark' ? 'white' : 'black' }}>
+                <h2>Your text summary :</h2>
+                <p>{text.trim().split(' ').length} Words, {text.length} Characters</p>
+                <p>{0.008 * text.trim().split(" ").length} Minutes Read</p>
+                <h2>Preview</h2>
+                <p>{text.length>0?text: "Enter something in the TextBox above to preview it here"}</p>
+            </div>
+        </>
+    )
+}
